f61306042bd58852b76c9ddbf459b2cab715f6c4
Osrosario <114501083+Osrosario@users.noreply.github.com>
2023-02-08 19:32:08 -0500

    Implemented Navigation Between Fragments

--- app/src/main/java/edu/quinnipiac/ser210/secretmessage/WelcomeFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/secretmessage/WelcomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -30,8 +33,15 @@ class WelcomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val startButton = view.findViewById<Button>(R.id.start)
+
+        startButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_welcomeFragment_to_messageFragment)
+        }
+
+        return view
     }
 
     companion object {
